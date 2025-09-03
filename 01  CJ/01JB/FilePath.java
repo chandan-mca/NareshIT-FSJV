e01249ee43255363dadd84f5d9b97938f9dda74e
Chandan Agrawal <cagrawal0012@gmail.com>
2025-09-03 20:32:09 +0530

    Ch-2 notes added

---
+++ 01  CJ/01JB/FilePath.java
@@ -0,0 +1,7 @@
+class FilePath {
+	public static void main(String[] args) {
+		System.out.println("D:\\abc\\xyz\\pqr.txt");
+		System.out.println("D:\\new\\batch\\snames.txt");
+	}
+}
+
---
+++ 01  CJ/02C,I,K/Comment1.java
@@ -0,0 +1,6 @@
+//Comments1.java
+class Comment1 {
+	public static void main(String[] args) {
+		System.out.println("Hi");
+	}
+}
