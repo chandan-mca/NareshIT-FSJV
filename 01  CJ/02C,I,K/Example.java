63049f6027c9fc011db3208e47ef5d1ffeec6b2f
Chandan Agrawal <cagrawal0012@gmail.com>
2025-09-07 20:36:56 +0530

    java code added

---
+++ 01  CJ/02C,I,K/Example.java
@@ -0,0 +1,13 @@
+class Example {
+	
+	static int x = 10;
+	
+	static void m1() {
+		System.out.println("M1");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(x);
+		m1();
+	}
+}
